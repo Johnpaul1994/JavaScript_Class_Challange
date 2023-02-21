d2bbe55c0fd01e6044bfddc82ee8e4a422b32a33
Johnpaul1994 <johnpaulnedolisa@gmail.com>
2023-02-22 00:21:45 +0100

    Push new

--- challangesolution.js
+++ challangesolution.js
@@ -1,41 +1,67 @@
 //question 1
-function multiply(a, b) {
-    return a * b;
-}
+// function multiply(a, b) {
+//     return a * b;
+// }
 
 // question 2
-function evenOrOdd(number) {
-    if (number % 2 === 0) {
-        return "even";
-    }
-    // if the nummber is odd
-    else {
-        return "Odd";
-    }
-}
+// function evenOrOdd(num) {
+//     if (num % 2 === 0) {
+//         return "even";
+//     }
+//     // if the nummber is odd
+//     else {
+//         return "Odd";
+//     }
+// }
 
 // question 3
-function positiveSum(arr){
-    let sum = 0;
-    for (i =0; i < arr.length; i++){
-        if (arr[i]>0){
-            sum += arr[i];
-        }
-    }
-    return sum;
-}
+// function positiveSum(arr){
+//     let sum = 0;
+//     for (i =0; i < arr.length; i++){
+//         if (arr[i]>0){
+//             sum += arr[i];
+//         }
+//     }
+//     return sum;
+// }
 
-// question 4
-function removeChar(arr) {
-    return structuredClone.substring(1, str.length -1);
-}
+// // // question 4
+// function removeChar(arr) {
+//     return structuredClone.substring(1, str.length -1);
+// }
 
-// question 5 
-let n = 0.34
-function makeNegative(num){
-    if(num<0){
-        return num;
-    } else {
-        return -num;
-    }
-}
+// // question 5 
+// let num = -9
+// function makeNegative(num){
+//     if(num<0){
+//         return num;
+//     } else{
+//         return -num;
+//     }
+       
+// }
+// console.log(makeNegative(num))
+
+// //question 6 
+// let s = 6
+// let i = "guy"
+// function repeatStr(s, i) {
+//     for(let j=0; j<s;j++){
+//         console.log[j];
+//     }
+// }
+// return(s, i)
+
+//question 7
+
+
+// // try catch error
+// //write a funtion that adds two numbers
+// function addTwoNums(a,b){
+//     try{
+
+//     } catch (error) {}
+// }
+
+//referenceError, when you are calling a variable that has not been decleared 
+
